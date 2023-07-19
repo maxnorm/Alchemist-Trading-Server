90ca0a54648998ea0d30ae08f69e1b10ba2ac7d9
maxnorm <maxnorm@hotmail.com>
2023-07-19 14:49:04 -0400

    ADD inactivity_manager.py (not finish)

---
+++ mt5-python_server/src/inactivity_manager.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from utils.market_utils import check_if_market_open
+
+
+class InactivityManager:
+    """
+    Class for managing socket inactivity
+    """
+
+    def __init__(self, minute_inactivity_allowed):
+        self.__minute_inactivity_allowed = minute_inactivity_allowed
+        self.__last_msg = None
+
+    def reset_time(self):
+        """
+        Set the datetime for when the last message was receive
+        """
+        self.__last_msg = datetime.now()
+
+    def is_inactive(self):
+        """
+        Check if the socket is inactive based on last message
+        """
+        span = datetime.now() - self.__last_msg
+        return check_if_market_open() and span.total_seconds() >= (self.__minute_inactivity_allowed * 60)
