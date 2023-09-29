916a5a42ff27e6f1dc774244c3ef74e2a9d403a0
Maxime Normandin <1720380@cstjean.qc.ca>
2023-09-28 23:13:45 -0400

    refactor: new abstraction for the mt5 connection

---
+++ mt5-python_server/src/mt5_connection/conn.py
@@ -0,0 +1,31 @@
+import json
+
+
+class Connection:
+    """
+    Class to connect to MT5 terminal via socket
+    """
+    def __init__(self, socket, stop_char='\n', verbose=False, console_lock=None):
+        self.__socket = socket
+        self.__stop_char = stop_char
+        self.__verbose = verbose
+        self.__console_lock = console_lock
+
+    def send_msg(self, msg):
+        """
+        Send a message to the terminal via socket by adding the stop character to end the message
+        :param msg: message to send
+        """
+        self.__socket.send(bytes(msg + self.__stop_char, 'utf-8'))
+
+    async def get_response(self):
+        """
+        Get the response in json fromat from MT5 terminal
+        """
+        cum_data = ''
+        while True:
+            data = self.__socket.recv(1024).decode("utf-8")
+
+            cum_data += data
+            if self.__stop_char in cum_data:
+                return json.loads(cum_data[:cum_data.index(self.__stop_char)])
--- mt5-python_server/src/mt5_connection/terminal.py
+++ mt5-python_server/src/mt5_connection/terminal.py
@@ -6,6 +6,7 @@ import json
 from codes.terminal_code import Terminal
 from codes.trade_request_code import TradeRequest
 from models.trade import Trade
+from mt5_connection.conn import Connection
 
 
 def generate_new_id():
@@ -18,40 +19,16 @@ def generate_new_id():
     return new_id
 
 
-class MT5Terminal:
+class MT5Terminal(Connection):
     """
     MT5 terminal connection for trading operation
     """
     last_id = 0
     __trade_request_executed_code = 10009
 
-    def __init__(self, socket, stop_char='\n',
-                 verbose=False, console_lock=None):
-        self.__socket = socket
-        self.__stop_char = stop_char
-        self.__verbose = verbose
-        self.__console_lock = console_lock
-
+    def __init__(self, socket, stop_char='\n', verbose=False, console_lock=None):
+        super().__init__(socket, stop_char, verbose, console_lock)
         self.id = generate_new_id()
-
-    def __send_msg(self, msg):
-        """
-        Send a message to the terminal via socket by adding the stop character to end the message
-        :param msg: Message to send
-        """
-        self.__socket.send(bytes(msg + self.__stop_char, 'utf-8'))
-
-    async def __get_response(self):
-        """
-        Get the response in json fromat from MT5 terminal
-        """
-        cum_data = ''
-        while True:
-            data = self.__socket.recv(1024).decode("utf-8")
-
-            cum_data += data
-            if self.__stop_char in cum_data:
-                return json.loads(cum_data[:cum_data.index(self.__stop_char)])
 
     async def get_all_infos(self):
         """
